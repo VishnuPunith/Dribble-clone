b98247f327f899046bce67cf61bd8ecfe7188c59
VishnuPuneethR <93135223+VishnuPunith@users.noreply.github.com>
2024-04-12 10:59:25 +0530

    footer add

--- pages/index.vue
+++ pages/index.vue
@@ -2235,8 +2235,28 @@
           </svg></span>
 
       </div>
+      
     </div>
-
+    <div class = "footer_demo">
+        <div class = "left_foot">
+          <ul>
+          <li>© 2024 Dribbble</li>
+          <li>Terms</li>
+          <li>Privacy</li>
+          <li>Cookies</li>
+        </ul>
+        </div>
+        <div class = "right_foot">
+          <ul>
+          <li>Jobs</li>
+          <li>Designers</li>
+          <li>Freelancers</li>
+          <li>Tags</li>
+          <li>Places</li>
+          <li>Resources</li>
+        </ul>
+        </div>
+      </div>
   </div>
 
 </template>
@@ -2263,7 +2283,31 @@ body {
   margin-left: 4px;
   z-index: 2;
 }
+.left_foot>ul{
+  display: flex;
+    justify-content: center;
+    gap: 4pc;
+    list-style: none;
+    font-size: 100%;
+    color: #6e6d7a;
+    margin-left: -42px;
+    font-weight: 400;
+}
+.right_foot>ul{
+  display: flex;
+    justify-content: center;
+    gap: 40px;
+    list-style: none;
+    font-size: 100%;
+    color: #6e6d7a;
+    font-weight: 400;
 
+}
+.footer_demo{
+  display: flex;
+  justify-content:space-between;
+  padding: 76px;
+}
 .cardtwo {
   background-color: #f6d9fb;
   width: 200px;
